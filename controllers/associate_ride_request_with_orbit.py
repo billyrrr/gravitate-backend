b2b6c1294a9bb9ddb0e9b9c9faee7f662b98c741
billyrrr <billrao@me.com>
2018-11-11 01:55:17 -0800

    grouping logics

--- controllers/associate_ride_request_with_orbit.py
+++ controllers/associate_ride_request_with_orbit.py
@@ -5,14 +5,39 @@ from data_access.orbit_dao import OrbitDao
 from google.cloud.firestore import *
 from usersHelper.placeInOrbit import placeInOrbit
 
-def joinOrbitToRideRequest(client: Client, rideRequestRef: DocumentReference, orbitRef: DocumentReference):
+
+def joinOrbitToRideRequest(client: Client, rideRequestRef: DocumentReference, preDecisionRideRequest: RideRequest,
+                           orbitRef: DocumentReference, preDecisionOrbit: Orbit):
+    """ Description
+    This function joins a rideRequest to an orbit in the database. 
+        Firstly, the function accesses database copy of the objects and download them as local copies
+        Secondly, the function validates that the database copy of the object matches those passed along by the decision maker[1]
+        Thirdly, the function modifies local copies so that a rideRequest is joined to an orbit
+        Fourthly, the function updates database copy of the objects, and throw an error if they changed since last read
+
+    [1] Note that decision maker should refresh local copies of the objects after each join, 
+        since this function will raise an exception if they don't match. 
+
+    :type client:Client:
+    :param client:Client:
+
+    :type rideRequestRef:DocumentReference:
+    :param rideRequestRef:DocumentReference:
+
+    :type orbitRef:DocumentReference:
+    :param orbitRef:DocumentReference:
+
+    :raises:
+
+    :rtype:
+    """
 
     # Create a transaction so that an exception is thrown when updating an object that is changed since last read from database
     transaction = client.transaction()
 
-
     rideRequestDao = RideRequestDao(client)
-    rideRequest = rideRequestDao.getRideRequestWithTransaction(transaction, rideRequestRef)
+    rideRequest = rideRequestDao.getRideRequestWithTransaction(
+        transaction, rideRequestRef)
     orbitDao = OrbitDao(client)
     orbit = orbitDao.getOrbitWithTransaction(transaction, orbitRef)
 
@@ -23,11 +48,13 @@ def joinOrbitToRideRequest(client: Client, rideRequestRef: DocumentReference, or
     placeInOrbit(rideRequest, orbit)
 
     # Update database copy of rideRequest and orbit
-    try: 
-        rideRequestDao.setRideRequestWithTransaction(transaction, rideRequest, rideRequest.firestoreRef)
-        orbitDao.setOrbitWithTransaction(transaction, orbit, orbit.firestoreRef)
+    try:
+        rideRequestDao.setRideRequestWithTransaction(
+            transaction, rideRequest, rideRequest.firestoreRef)
+        orbitDao.setOrbitWithTransaction(
+            transaction, orbit, orbit.firestoreRef)
         # TODO: add other works to be done if any
         transaction.commit()
     except:
-        # Firestore rollsback operations automatically. No need for manual rollback. 
+        # Firestore rollsback operations automatically. No need for manual rollback.
         raise
---
+++ controllers/grouping.py
@@ -0,0 +1,49 @@
+from controllers.associate_ride_request_with_orbit import joinOrbitToRideRequest
+from models.orbit import Orbit
+from google.cloud.firestore import Client
+
+client = Client()
+
+def groupRideRequests():
+    
+    
+    """ Description
+        [Not Implemented]
+        This function 
+        1. reads all ride requests associated with an event
+        2. puts ride requests into groups
+        3. call join method on each group
+    :raises:
+
+    :rtype:
+    """
+    
+    # TODO Implement
+
+    pass
+
+class Group:
+
+    def __init__(self, rideRequestArray:[]):
+        self.rideRequestArray = rideRequestArray
+
+        # Note that the intended orbit will be in database, and hence possible to be modified by another thread
+        self.intendedOrbit = None # TODO create an orbit (may need a factory pattern) and add to database
+
+    def doWork(self):
+        orbit = self.intendedOrbit
+
+        # Record which users failed join the orbit
+        notJoined = []
+
+        for rideRequest in self.rideRequestArray:
+            try:
+                # Trying to join one rideRequest to the orbit
+                joinOrbitToRideRequest(client, rideRequest.firestoreRef, rideRequest, orbit.firestoreRef, orbit)
+            except:
+                # TODO when failing to join, move on to next
+                notJoined.append(rideRequest)
+                raise
+
+        return notJoined
+        
