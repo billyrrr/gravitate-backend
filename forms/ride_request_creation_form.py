d74f6f7102251563f8aa50079da67cd935271810
Leon <lw75251@gmail.com>
2018-11-16 19:22:48 -0800

    RideRequest Form

--- forms/ride_request_creation_form.py
+++ forms/ride_request_creation_form.py
@@ -1,7 +1,42 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, PasswordField, DateTimeField
+#### DateTimeField does not have Timezone #### 
+
+
+from wtforms.validators import InputRequired,length
 
 class RideRequestCreationForm(Form):
-    pickupAddress = StringField(validators=[InputRequired('Pickup address needs to be specified. '))
+    # Flight Information (can be filled with Flightstats API)
+    pickupAddress = StringField(validators=[
+        InputRequired('Pickup address needs to be specified.'),
+        
+        
+        ])
+    airportLocation = StringField(validators=[
+        InputRequired('Airport Location needs to be specified.'),
+        
+        ])
+    flightLocalTime = DateTimeField(validators=[
+        InputRequired('Flight Local Time needs to be specified. '),
+        
+        ])
+
+    # Used to create Target Object
+    earliest = DateTimeField(validators=[
+        InputRequired('Earliest Arrival needs to be specified.'),
+        
+        ])
+
+    latest = DateTimeField(validators=[
+        InputRequired('Latest Arrival needs to be specified. '),
+        
+        ])
+
+
+    # TODO: Validate Airport
+    def validate_airport(form, airportLocation) {
+        # Find Airport Code Library
+        if airportLocation not in PACKAGE_NAME:
+            raise ValidationError("Not a valid Airport Code")
+    }
     
