af9cf19bd9fa35d9bbeab8891da3f38c8fbf0306
Andrew Kim <andrewykim528@gmail.com>
2018-11-21 17:32:00 -0800

    updated event.py in models

--- models/event.py
+++ models/event.py
@@ -16,4 +16,56 @@ class Event(object):
 
 	def getFirestoreRef(self):
 		return self.__firestoreref
+	@staticmethod
+	def fromDictAndReference(eventDict, eventRef):
+		event = Event.fromDict(eventDict)
+		event.setFirestoreRef(eventRef)
+		return event
 
+	def fromDict(eventDict):
+		""" Description
+		    This function creates an event
+		    
+		    :param eventDict:
+		"""
+		eventRef = eventDict['eventRef']
+		eventCategory = eventDict['eventCategory']
+		participants = eventDict['particapants']
+		eventLocation = eventDict['eventLocation']
+		startTimeStamp = eventDict['startTimeStamp']
+		endTimeStamp = eventDict['endTimeStamp']
+		pricing = eventDict['pricing']
+		return Event(eventRef, eventCategory, participants, eventLocation, startTimeStamp, endTimeStamp, pricing)
+
+	def toDict(self):
+		eventDict = {
+			'eventRef' = self.eventRef
+			'eventCategory' = self.eventCategory
+			'participants' = self.participants
+			'eventLocation' = self.eventLocation
+			'startTimeStamp' = self.startTimeStamp
+			'endTimeStamp' = self.endTimeStamp
+			'pricing' = self.pricing
+		}
+		return eventDict
+
+	def __init__(self, eventRef, eventCategory, participants, eventLocation, startTimeStamp, endTimeStamp, pricing):
+		"""Description
+		   This function initializes an Event object
+
+		   :param self:
+		   :param eventRef:
+		   :param eventCategory:
+		   :param participants:
+		   :param eventLocation:
+		   :param startTimeStamp:
+		   :param endTimeStamp:
+		   :param pricing:
+		"""
+		self.eventRef = eventRef
+		self.eventCategory = eventCategory
+		self.participants = participants
+		self.eventLocation = eventLocation
+		self.startTimeStamp = startTimeStamp
+		self.endTimeStamp = endTimeStamp
+		self.pricing = pricing
