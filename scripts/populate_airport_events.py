02ae93cf54e43ca9f653993b9a4dbe35dd1ceac4
billyrrr <billrao@me.com>
2018-11-29 00:25:57 -0800

    added script for adding 24-hour airport events in firestore

--- scripts/populate_airport_events.py
+++ scripts/populate_airport_events.py
@@ -1,4 +1,10 @@
 from models.event import Event
+from data_access.event_dao import EventDao
+import datetime
+import pytz
+import iso8601
+import warnings
+
 
 class EventBuilder(Event):
 
@@ -14,12 +20,13 @@ class EventBuilder(Event):
     def buildLists(self):
         # Since we are unsure of how participants and locationRefs will be represented
         raise NotImplementedError
-    
+
     def buildTimeRange(self):
         raise NotImplementedError
 
     def buildExtraInfo(self):
         raise NotImplementedError
+
 
 class LaxEventBuilder(EventBuilder):
 
@@ -33,6 +40,7 @@ class LaxEventBuilder(EventBuilder):
 
     def buildExtraInfo(self):
         self.pricing = 100
+
 
 class SampleLaxEventBuilder(LaxEventBuilder):
 
@@ -54,4 +62,71 @@ class SpecifiedRangeLaxEventBuild(LaxEventBuilder):
         self.endTimestamp = endTimestamp
 
 
+def generateStartDatetime(startDayDatetimeStr: str) -> datetime.datetime:
+    """ Description
+    :type startDayDatetimeStr:str:
+    :param startDayDatetimeStr:str: ie. "2018-12-17T08:00:00.000"
 
+    :raises:
+
+    :rtype:
+    """
+    tz = pytz.timezone('America/Los_Angeles')
+
+    startDayDatetime = iso8601.parse_date(
+        startDayDatetimeStr, default_timezone=None).astimezone(tz)
+
+    # Represents "2018-12-17T00:00:00.000" 'America/Los_Angeles'
+    startDatetime: datetime.datetime = datetime.datetime(
+        startDayDatetime.year, startDayDatetime.month, startDayDatetime.day, tzinfo=None).astimezone(tz)
+
+    return startDatetime
+
+
+def generateTimestamps(startDatetime: datetime.datetime, numDays: int) -> [(int, int)]:
+
+    curStart = startDatetime
+    curNumDays = 0
+    tupleList = list()
+
+    while (curNumDays < numDays):
+        endDatetime = curStart + \
+            datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+
+        # Handles the case where 1 day after startDatetime and 1 second before is still tomorrow
+        # (which is not expected to occur in California)
+        while (endDatetime.day != curStart.day):
+            warnings.warn(
+                "1 day after startDatetime and 1 second before is still tomorrow. curStart = {}".format(curStart))
+            assert startDatetime.timestamp() < endDatetime.timestamp()
+            endDatetime = endDatetime - datetime.timedelta(seconds=1)
+
+        startTimestamp = int(curStart.timestamp())
+        endTimestamp = int(endDatetime.timestamp())
+        tupleList.append((startTimestamp, endTimestamp))
+
+        # Increase counter and curStart
+        curNumDays += 1
+        curStart = curStart + datetime.timedelta(days=1)
+
+    return tupleList
+
+
+def generateEvents(timestampTupleList: list):
+
+    eventList = list()
+
+    for startTimestamp, endTimestamp in timestampTupleList:
+        newEvent = SpecifiedRangeLaxEventBuild(startTimestamp, endTimestamp)
+        eventList.append(newEvent)
+
+    return eventList
+
+
+def populateEvents(startString="2018-12-07T08:00:00.000", numDays=35):
+    startDatetime = generateStartDatetime(startString)
+    timestampTupleList = generateTimestamps(startDatetime, numDays)
+    eventList = generateEvents(timestampTupleList)
+    for event in eventList:
+        eventRef = EventDao().create(event)
+        print(eventRef)
--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,7 +1,7 @@
 import unittest
 
 from scripts.populate_locations import buildLaxTerminal
-from scripts.populate_airport_events import SampleLaxEventBuilder
+from scripts.populate_airport_events import SampleLaxEventBuilder, generateStartDatetime, generateTimestamps, generateEvents
 
 
 class TestBuildLaxTerminal(unittest.TestCase):
@@ -27,3 +27,23 @@ class TestBuildLaxEvent(unittest.TestCase):
     def testBuildLaxSampleEvent(self):
         laxSampleEvent = SampleLaxEventBuilder()
         self.assertDictEqual(laxSampleEvent.toDict(), sampleLaxEventDict)
+
+    def testGenerateStartDatetime(self):
+        generateStartDatetime("2018-12-17T08:00:00.000")
+
+    def testGenerateTimestamps(self):
+        startDatetime = generateStartDatetime("2018-12-17T08:00:00.000")
+        timestampTupleList = generateTimestamps(startDatetime, 2)
+        for startTimestamp, endTimestamp in timestampTupleList:
+            print("start: {}, end: {}".format(startTimestamp, endTimestamp))
+
+
+class TestGenerateEvent(unittest.TestCase):
+
+    def testGenerateEvent(self):
+        startDatetime = generateStartDatetime("2018-12-01T08:00:00.000")
+        timestampTupleList = generateTimestamps(startDatetime, 2)
+        eventList = generateEvents(timestampTupleList)
+        expectedDay2EventDict = {'eventCategory': 'airport', 'participants': [], 'eventLocation': 'LAX',
+                             'startTimestamp': 1543737600, 'endTimestamp': 1543823999, 'pricing': 100, 'locationRefs': []}
+        self.assertDictEqual(eventList[1].toDict(), expectedDay2EventDict)
