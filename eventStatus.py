d89334f550c267406a956ef40d095cbd1116fad5
Andrew Kim <ayk038@ucsd.edu>
2018-11-30 20:05:37 -0800

    eventStatus.py now checks whether event's status is currently occuring and changes it

--- eventStatus.py
+++ eventStatus.py
@@ -1,3 +1,18 @@
 #Author: Andrew Kim
 
+from models.event import Event
 
+event: Event
+
+class EventStatus:
+	
+	@staticmethod
+	def eventStatus(timeAfterFlight):
+		""" Definition
+		    Sets the event to a past event category one day after the start time
+		    :param timeAfterFlight:
+		"""
+		#check if eventStatus is True before proceeding
+		if event.eventStatus = True:
+			if event.startTimestamp >= event.startTimeStamp + timeAfterFlight:
+				event.eventStatus = False
