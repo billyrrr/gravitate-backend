c764092eaa2c40e25f5c14d8e478c580d5e9230f
Andrew Kim <ayk038@ucsd.edu>
2018-11-30 20:15:51 -0800

    added isClosed variable to event.py instead of eventStatus

--- eventStatus.py
+++ eventStatus.py
@@ -1,18 +1,19 @@
 #Author: Andrew Kim
-
+import datetime
 from models.event import Event
 
 event: Event
+ts = datetime.datetime.now().timestamp()
 
 class EventStatus:
 	
 	@staticmethod
-	def eventStatus(timeAfterFlight):
+	def eventStatus(timeAfterEventStart):
 		""" Definition
 		    Sets the event to a past event category one day after the start time
 		    :param timeAfterFlight:
 		"""
 		#check if eventStatus is True before proceeding
-		if event.eventStatus = True:
-			if event.startTimestamp >= event.startTimeStamp + timeAfterFlight:
-				event.eventStatus = False
+		if event.isClosed = False:
+			if ts >= event.startTimestamp + timeAfterEventStart:
+				event.isClosed = True
--- models/event.py
+++ models/event.py
@@ -40,7 +40,8 @@ class Event(object):
 		endTimestamp = eventDict['endTimestamp']
 		pricing = eventDict['pricing']
 		locationRefs = eventDict['locationRefs']
-		return Event(eventCategory, participants, eventLocation, startTimestamp, endTimestamp, pricing, locationRefs)
+		isClosed = eventDict['isClosed']
+		return Event(eventCategory, participants, eventLocation, startTimestamp, endTimestamp, pricing, locationRefs, isClosed)
 
 	def toDict(self):
 		eventDict = {
@@ -51,25 +52,26 @@ class Event(object):
 			'endTimestamp': self.endTimestamp,
 			'pricing': self.pricing,
 			'locationRefs': self.locationRefs
+			'isClosed': self.isClosed
 		}
 		return eventDict
 	def setEventAsActive(self):
 		""" Definition
-		    Sets the eventStatus to True
+		    Sets the boolean isClosed to False
 		    
 		    :param self:
 		"""
-		self.eventStatus = True
+		self.isClosed = False
 
 	def setEventAsPassed(self):
 		""" Definition
-		    Sets the eventStatus to False
+		    Sets the boolean isClosed to True
 		   
 		    :param self:
 		"""
-		self.eventStatus = False
+		self.isClosed = True
 
-	def __init__(self, eventCategory, participants, eventLocation, startTimestamp, endTimestamp, pricing, locationRefs):
+	def __init__(self, eventCategory, participants, eventLocation, startTimestamp, endTimestamp, pricing, locationRefs, isClosed):
 		"""Description
 		   This function initializes an Event object
 
@@ -81,6 +83,7 @@ class Event(object):
 		   :param endTimestamp:
 		   :param pricing:
 		   :param locationRefs: a list of locationRef that corresponds to this event
+		   :param isClosed: 
 		"""
 		self.eventCategory = eventCategory
 		self.participants = participants
@@ -89,3 +92,4 @@ class Event(object):
 		self.endTimestamp = endTimestamp
 		self.pricing = pricing
 		self.locationRefs = locationRefs
+		self.isClosed = isClosed
