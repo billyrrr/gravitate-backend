72d2ea4c4197ca3c1d2e236f7db37ad4830ac2db
Andrew Kim <ayk038@ucsd.edu>
2018-12-01 22:29:14 -0800

    changed logic in eventStatus.py

---
+++ eventStatus.py
@@ -0,0 +1,19 @@
+#Author: Andrew Kim
+import datetime
+from models.event import Event
+
+event: Event
+ts = datetime.datetime.now().timestamp()
+
+class EventStatus:
+	
+	@staticmethod
+	def eventStatus():
+		""" Definition
+		    Sets the event to a past event category one day after the start time 
+		"""
+		#check if eventStatus is True before proceeding
+		if event.isClosed == False:
+			if ts >= event.endTimestamp:
+				event.isClosed = True
+
