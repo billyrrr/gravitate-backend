144f4fc603c774a1b609227cd0f567ba34f333b5
billyrrr <billrao@me.com>
2018-12-02 16:56:31 -0800

    added grouping and simplified code

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Import Model Classes
+from models.ride_request import RideRequest, AirportRideRequest, SocialEventRideRequest
+from models.location import Location, AirportLocation
+from models.event import Event
+from models.orbit import Orbit
+from models.target import Target, ToEventTarget
+from models.user import User
