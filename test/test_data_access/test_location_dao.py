752d1b5059480d82f7891805e21a25180fc0e6c0
billyrrr <billrao@me.com>
2018-12-15 20:10:39 -0800

    add experimental LocationGenericDao methods

--- test/test_data_access/test_location_dao.py
+++ test/test_data_access/test_location_dao.py
@@ -1,5 +1,10 @@
 import unittest
 from gravitate.data_access import LocationGenericDao
+from google.cloud.firestore import Transaction, transactional
+from gravitate import config
+import test.factory.model as model
+
+db = config.Context.db
 
 
 class LocationDAOTest(unittest.TestCase):
@@ -10,3 +15,13 @@ class LocationDAOTest(unittest.TestCase):
     def testFindByAirportCode(self):
         result = LocationGenericDao().findByAirportCode('LAX')
         self.assertNotEqual(None, result, 'Should return a result. ')
+
+    def testSetWithTransaction(self):
+        transaction = db.transaction()
+        LocationGenericDao.setWithTransaction(transaction, model.getLocation(), model.mock1["locationFirestoreRef"])
+        transaction.commit()
+
+    def testSetWithTransactionTransactional(self):
+        transaction = db.transaction()
+        LocationGenericDao.setWithTransactionTransactional(transaction, model.getLocation(), model.mock1["locationFirestoreRef"])
+        # transaction.commit()
