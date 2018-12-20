eacaa94ef1eb4c0f3373ff874b003ccc5e6e6681
billyrrr <billrao@me.com>
2018-12-20 20:41:28 +0800

    Relocate authenticate decorator

--- gravitate/main.py
+++ gravitate/main.py
@@ -17,12 +17,10 @@
 # [START gae_flex_quickstart]
 import logging
 
-from flask import Flask, request, Response
-from flask_restful import reqparse, Api, Resource, abort
+from flask import Flask, request
+from flask_restful import reqparse, Api, Resource
 
 from google.auth.transport import requests
-
-from functools import wraps, partial
 
 # Firebase Admin SDK
 
@@ -36,8 +34,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from gravitate.services.grouping_service import OrbitForceMatchService, refreshGroupAll
 from gravitate.services.ride_request_service import RideRequestService, DeleteMatchService, DeleteRideRequestService
 from gravitate.services.user_service import UserService
+from gravitate.services.utils import authenticate
 
-TESTING = True
 
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(refreshGroupAll, 'interval', minutes=1)
@@ -49,70 +47,12 @@ app = Flask(__name__)
 db = Context.db
 parser = reqparse.RequestParser()
 
-# For Testing Purposes
-auth_verify_id_token = None
-if TESTING:
-    def mock_auth_verify_id_token(*args, **kwargs):
-        return {
-            "uid": "testuid1"
-        }
-    auth_verify_id_token = mock_auth_verify_id_token
-else:
-    auth_verify_id_token = auth.verify_id_token
 
 
-def default_gravitate_authentication(id_token) -> (str, int):
-    try:
-        # Verify the ID token while checking if the token is revoked by
-        # passing check_revoked=True.
-        decoded_token = auth_verify_id_token(id_token, check_revoked=True, app=Context.firebaseApp)
-        # Token is valid and not revoked.
-        uid = decoded_token['uid']
-        return uid, 200
-    except auth.AuthError as exc:
-        if exc.code == 'ID_TOKEN_REVOKED':
-            # Token revoked, inform the user to reauthenticate or signOut().
-            return None, 401
-        else:
-            # Token is invalid
-            return None, 402
+class EndpointTestService(Resource):
 
+    method_decorators = [authenticate]
 
-def authenticate(func):
-    """
-        Wraps a resource to provide authentication.
-        Note that the resource need to take uid in kwargs
-    :param func:
-    :return:
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if not getattr(func, 'authenticated', True):
-            return func(*args, **kwargs)
-
-        id_token = request.headers['Authorization'].split(' ').pop()
-        uid, status_code = default_gravitate_authentication(id_token)  # custom account lookup function
-
-        if status_code == 401:
-            abort(401, 'Unauthorized. Token revoked, inform the user to reauthenticate or signOut(). ')
-        elif status_code == 402:
-            abort(402, 'Invalid token')
-
-        if uid:
-            func_acct = partial(func, uid=uid)
-            return func_acct(*args, **kwargs)
-
-        abort(401)
-
-    return wrapper
-
-
-class AuthenticatedResource(Resource):
-    method_decorators = [authenticate]  # applies to all inherited resources
-
-
-class EndpointTestService(AuthenticatedResource):
     def post(self, uid):
         """
         * This method handles a POST/PUT call to './authTest' to test that front end Auth
@@ -124,7 +64,6 @@ class EndpointTestService(AuthenticatedResource):
         """
 
         # Verify Firebase auth.
-
         data = request.get_json()
         responseDict = {'uid': uid, 'request_data': data}
         return responseDict, 200
