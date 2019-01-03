c5b40456760d8bb8d550106564ab2cf736418410
billyrrr <billrao@me.com>
2019-01-03 12:30:38 -0800

    Change module structure

--- gravitate/config.py
+++ gravitate/config.py
@@ -32,19 +32,19 @@ class GravitateConfig:
 class DevelopmentGravitateConfig(GravitateConfig):
     DEBUG = True
     TESTING = False
-    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/gravitate-dev-firebase-adminsdk-79k5b-04b4ed676d.json"
+    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/config_jsons/gravitate-dev-firebase-adminsdk-79k5b-04b4ed676d.json"
     APP_NAME = "gravitate-dev"
 
 
 class TestingGravitateConfig(GravitateConfig):
     DEBUG = True
     TESTING = True
-    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/gravitate-dev-firebase-adminsdk-79k5b-04b4ed676d.json"
+    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/config_jsons/gravitate-dev-firebase-adminsdk-79k5b-04b4ed676d.json"
     APP_NAME = "gravitate-dev"
 
 
 class StagingGravitateConfig(GravitateConfig):
-    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/gravitate-e5d01-firebase-adminsdk-kq5i4-943fb267ce.json"
+    FIREBASE_CERTIFICATE_JSON_PATH = "gravitate/config_jsons/gravitate-e5d01-firebase-adminsdk-kq5i4-943fb267ce.json"
     APP_NAME = "gravitate-e5d01"
     DEBUG = False
     TESTING = False
---
+++ gravitate/controllers/event_status.py
@@ -0,0 +1,22 @@
+"""
+Author: Andrew Kim
+"""
+
+import datetime
+from gravitate.models import Event
+
+
+def refresh_event_status(event: Event):
+    """ Definition
+        Sets the event to a past event category one day after the start time
+    """
+    ts = datetime.datetime.now().timestamp()
+    # check if refresh_event_status is True before proceeding
+    if not event.is_closed and ts >= event.end_timestamp:
+        return True
+    else:
+        return False
+
+
+def close_event(event: Event):
+    event.is_closed = True
