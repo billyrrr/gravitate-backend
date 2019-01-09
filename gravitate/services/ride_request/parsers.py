75db0b0c1d4b3646ab3569e8e31edfbf2d028c4a
billyrrr <billrao@me.com>
2019-01-08 21:17:55 -0800

    Refactor parsers

--- gravitate/services/ride_request/parsers.py
+++ gravitate/services/ride_request/parsers.py
@@ -5,25 +5,25 @@ Switch to reqparse to reduce repeated code for different types of rideRequest
 
 from flask_restful import reqparse
 
-# Parse field values from url path
-airport_parser = reqparse.RequestParser(bundle_errors=True)
+# Parse field values from json
+ride_request_base_parser = reqparse.RequestParser(bundle_errors=True)
+ride_request_base_parser.add_argument('pickupAddress', type=str, help='Pickup Address', location="json")
+ride_request_base_parser.add_argument('toEvent', type=bool, help='whether the ride is heading to the event',
+                                      location="json")
+ride_request_base_parser.add_argument('driverStatus', type=bool,
+                                      help="whether the user want to be considered as a driver for the event",
+                                      location="json")
+
+# Parse field values from json
+airport_parser = ride_request_base_parser.copy()
 airport_parser.add_argument('flightNumber', type=str, help='Flight Number', location="json")
 airport_parser.add_argument('airportCode', type=str, help='Airport Code', location="json")
 airport_parser.add_argument('flightLocalTime', type=str, help='Flight Local Time ISO8601', location="json")
-airport_parser.add_argument('pickupAddress', type=str, help='Pickup Address', location="json")
-airport_parser.add_argument('toEvent', type=bool, help='whether the ride is heading to the event', location="json")
-airport_parser.add_argument('driverStatus', type=bool,
-                            help="whether the user want to be considered as a driver for the event", location="json")
 
-
-social_event_ride_parser = reqparse.RequestParser(bundle_errors=True)
+# Parse field values from json
+social_event_ride_parser = ride_request_base_parser.copy()
 social_event_ride_parser.add_argument('eventId', type=str, help="Event ID", location="json")
-social_event_ride_parser.add_argument('pickupAddress', type=str, help='Pickup Address', location="json")
-social_event_ride_parser.add_argument('toEvent', type=bool, help='whether the ride is heading to the event',
-                                      location="json")
-social_event_ride_parser.add_argument('driverStatus', type=bool,
-                                      help="whether the user want to be considered as a driver for the event",
-                                      location="json")
+
 
 """
 RideRequest creation useCase: FL-3: Connecting Flight Using Flight Number
