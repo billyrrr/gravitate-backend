a6179bac1ef00259ef9397f9b435c2e0c0743da3
billyrrr <billrao@me.com>
2019-02-02 16:41:58 -0800

    Move logic for creating ride request to controllers module and rename modules

--- gravitate/services/ride_request/services.py
+++ gravitate/services/ride_request/services.py
@@ -11,12 +11,10 @@ from flask import request
 from flask_restful import Resource
 
 from gravitate.context import Context
-from gravitate.controllers import utils
 from gravitate.controllers.grouping import grouping
-from gravitate.data_access import RideRequestGenericDao, UserDao, EventScheduleGenericDao, LocationGenericDao
-from gravitate.models import AirportRideRequest
+from gravitate.controllers import ride_request_controller
+from gravitate.data_access import RideRequestGenericDao, UserDao, EventScheduleGenericDao
 import gravitate.services.utils as service_utils
-import gravitate.controllers.ride_request.utils as creation_utils
 from . import parsers as ride_request_parsers
 from gravitate.services import errors as service_errors
 
@@ -43,28 +41,7 @@ class AirportRideRequestCreationService(Resource):
         #     return errorResponseDict, 400
 
         # Create RideRequest Object
-        builder = creation_utils.AirportRideRequestBuilder()
-        ride_request: AirportRideRequest = builder \
-            .set_with_form_and_user_id(args, user_id) \
-            .build_airport_ride_request() \
-            .export_as_class(AirportRideRequest)
-        location = LocationGenericDao().get(ride_request.airport_location)
-
-        # Do Validation Tasks before saving rideRequest
-        # 1. Check that rideRequest is not submitted by the same user
-        #       for the flight on the same day already
-        # TODO: move to transactional logic for better atomicity
-        if utils.check_duplicate(ride_request.user_id, ride_request.event_ref):
-            raise service_errors.RequestAlreadyExistsError
-
-        # Starts database operations to (save rideRequest and update user's eventSchedule)
-        transaction = db.transaction()
-
-        # Transactional business logic for adding rideRequest
-        utils.add_ride_request(transaction, ride_request, location, user_id)
-
-        # Save write result
-        transaction.commit()
+        ride_request = ride_request_controller.create(args, user_id)
 
         # rideRequest Response
         response_dict = {
