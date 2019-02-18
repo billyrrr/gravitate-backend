c626b5df5683276c4e265da1e0a4ace40fce1e62
billyrrr <billrao@me.com>
2019-02-17 17:31:12 -0800

    Fix json import error

--- test/test_models/test_ride_request.py
+++ test/test_models/test_ride_request.py
@@ -14,21 +14,7 @@ class RideRequestTest(unittest.TestCase):
 
     def setUp(self):
         self.db = context.Context.db
-        self.rideRequestData1 = store.get_json_file('rideRequest_1.json')
         self.ride_request_data_2 = store.getMockRideRequest(returnDict=True)
-
-    def testAfterDictEqualsOriginalDict(self):
-        """ Tests that to_dict returns the same dict as the passed to from_dict.
-
-        :return:
-        """
-        ride_request_dict = self.rideRequestData1['rideRequest']
-        ride_request_ref = self.rideRequestData1['rideRequestRef']
-        airport_ride_request: AirportRideRequest = RideRequest.from_dict_and_reference(ride_request_dict,
-                                                                                       ride_request_ref)
-        result = airport_ride_request.to_dict()
-        self.assertDictEqual(ride_request_dict, result,
-                             "The dictionaries should equal if RideRequest was not modified. ")
 
     def testAfterDictEqualsOriginalDict2(self):
         """ Tests that to_dict returns the same dict as the passed to from_dict.
