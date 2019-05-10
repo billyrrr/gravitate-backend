822c4a701590d68500732c91b368bfb6ed9b220b
billyrrr <billrao@me.com>
2019-05-10 14:33:47 -0700

    Move create_fb_event

--- gravitate/domain/event/actions.py
+++ gravitate/domain/event/actions.py
@@ -1,7 +1,17 @@
 from gravitate.api_server.event import parsers as event_parser
+from gravitate.domain.event import builders_new as event_builders, models as event_models
+from gravitate.domain.event.dao import EventDao
+from gravitate.domain.user import UserDao
 
 
 def create(args, user_id, event_category="social"):
+    """ Creates event in the database.
+
+    :param args:
+    :param user_id:
+    :param event_category:
+    :return:
+    """
     if event_category == "social":
         return _create_social_event(args, user_id)
     else:
@@ -17,3 +27,25 @@ def _create_social_event(event_dict: dict, user_id):
     """
     p = event_parser.social_event_parser.parse_args()
     event_dict = p.values()
+
+
+def create_fb_event(event_dict, uid):
+    """ Creates event from a facebook event.
+
+    :param event_dict: event json returned by Facebook graph api
+    :param uid: user id
+    :return: event id just created
+    """
+    b = event_builders.FbEventBuilder()
+    # print(event_dict)
+    b.build_with_fb_dict(event_dict)
+    e: event_models.SocialEvent = b.export_as_class(event_models.SocialEvent)
+    # Note that e.firestore_ref will not be set by create()
+    ref = EventDao().create_fb_event(e)
+    e.set_firestore_ref(ref)
+    dict_view = e.to_dict_view()
+    dict_view["eventId"] = ref.id
+    # TODO: add error handling
+    UserDao().add_user_event_dict(uid, dict_view["fbEventId"], dict_view)
+    event_id = ref.id
+    return event_id
