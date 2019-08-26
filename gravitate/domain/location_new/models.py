692ffb11ec7fd7a6737e0e2f03bd7e0448f377b5
billyrrr <billrao@me.com>
2019-08-25 18:26:08 -0700

    Add location models based on flask_boiler (experimental)

---
+++ gravitate/domain/location_new/models.py
@@ -0,0 +1,141 @@
+from flask_boiler import schema, fields, domain_model
+from gravitate.domain.driver_navigation.utils import get_coordinates, \
+    get_address
+
+Schema = schema.Schema
+
+
+class CoordinateSchema(Schema):
+    latitude = fields.Raw(load_from="latitude", dump_to="latitude")
+    longitude = fields.Raw(load_from="longitude", dump_to="longitude")
+
+
+class LocationSchema(Schema):
+
+    coordinates = fields.Nested(CoordinateSchema,
+                                load_from="coordinates",
+                                dump_to="coordinates")
+    address = fields.Raw(load_from="address",
+                         dump_to="address")
+
+
+class Location(domain_model.DomainModel):
+
+    _schema_cls = LocationSchema
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.coordinates = dict()
+        self.address = str()
+
+
+class UserLocationSchema(LocationSchema):
+    pass
+
+
+class UserLocation(Location):
+
+    _schema_cls = UserLocationSchema
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class SocialEventLocationSchema(LocationSchema):
+
+    event_name = fields.Raw(
+        load_from="eventName",
+        dump_to="eventName")
+
+
+class SocialEventLocation(Location):
+
+    _schema_cls = SocialEventLocationSchema
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event_name = str()
+
+
+class UcLocationSchema(LocationSchema):
+
+    campus_code = fields.Raw(load_from="campusCode", dump_to="campusCode")
+    campus_name = fields.Raw(load_from="campusName", dump_to="campusName")
+
+
+class UcLocation(Location):
+
+    _schema_cls = UcLocationSchema
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.campus_code = str()
+        self.campus_name = str()
+
+
+class AirportLocationSchema(LocationSchema):
+
+    airport_code = fields.Raw(load_from="airportCode", dump_to="airportCode")
+
+
+class AirportLocation(Location):
+
+    _schema_cls = AirportLocationSchema
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.airport_code = str()
+
+
+campus_code_table = {
+    "UCSB": {
+        "obj_type": "UcLocation",
+        "coordinates": {
+            "latitude": 34.414132,
+            "longitude": -119.848868
+        },
+        "address": "C572+HC Isla Vista, California",
+        "campusName": "University of California, Santa Barbara",
+        "campusCode": "UCSB"
+    }
+}
+
+
+class LocationFactory:
+
+    @staticmethod
+    def from_pickup_address(pickup_address):
+        coordinates = get_coordinates(pickup_address)
+        obj = UserLocation.create()
+        obj.coordinates = coordinates
+        obj.address = pickup_address
+
+    @staticmethod
+    def from_code(code, location_category="campus"):
+        if location_category == "campus" and code in campus_code_table.keys():
+            return UcLocation.from_dict(campus_code_table[code])
+
+    @staticmethod
+    def from_fb_place(d):
+        """
+        Create location with facebook event - place
+        Example: {
+                "name": "Coachella",
+                "location": {
+                    "latitude": 33.679974,
+                    "longitude": -116.237221
+                },
+                "id": "20281766647"
+            }
+        :return:
+        """
+        address = get_address(d["location"])
+
+        obj = SocialEventLocation.create()
+
+        obj.coordinates=d["location"]
+        obj.address=address
+        obj.event_name=d["name"]
+
+        return obj
+
---
+++ test/test_domain/test_locations_new/test_models.py
@@ -0,0 +1,87 @@
+from gravitate.domain.location_new import models
+from unittest import TestCase
+
+
+class UserLocationModelTest(TestCase):
+
+    def test_from_dict(self):
+
+        d = {
+            'obj_type': "UserLocation",
+            'coordinates': {'latitude': 32.8794203,
+                                'longitude': -117.2428555},
+            'address': 'Tenaya Hall, San Diego, CA 92161',
+        }
+
+        obj = models.UserLocation.from_dict(d, doc_id=None)
+
+        self.assertDictEqual(obj.coordinates, {'latitude': 32.8794203,
+                                'longitude': -117.2428555})
+        self.assertEqual(obj.address, 'Tenaya Hall, San Diego, CA 92161')
+        self.assertTrue(isinstance(obj, models.UserLocation))
+
+
+class AirportLocationModelTest(TestCase):
+
+    def test_from_dict(self):
+        d = {
+            'obj_type': "AirportLocation",
+            'coordinates': {
+                "latitude": 33.9416,
+                "longitude": -118.4085
+            },
+            'address': "1 World Way, Los Angeles, CA 90045",
+            'airportCode': "LAX",
+        }
+        obj = models.AirportLocation.from_dict(d, doc_id=None)
+        self.assertDictEqual(obj.coordinates, {
+            "latitude": 33.9416,
+            "longitude": -118.4085
+        })
+        self.assertEqual(obj.address, "1 World Way, Los Angeles, CA 90045")
+        self.assertEqual(obj.airport_code, "LAX")
+        self.assertTrue(isinstance(obj, models.AirportLocation))
+
+
+class SocialEventLocationModelTest(TestCase):
+
+    def test_from_dict(self):
+        d = {
+            'obj_type': "SocialEventLocation",
+            'coordinates': {
+                "latitude": 33.9416,
+                "longitude": -118.4085
+            },
+            'address': "3150 Paradise Rd, Las Vegas, NV 89109",
+            "eventName": "CES"
+        }
+        obj = models.SocialEventLocation.from_dict(d, doc_id=None)
+        self.assertDictEqual(obj.coordinates, {
+                "latitude": 33.9416,
+                "longitude": -118.4085
+            })
+        self.assertEqual(obj.address, "3150 Paradise Rd, Las Vegas, NV 89109")
+        self.assertEqual(obj.event_name, "CES")
+        self.assertTrue(isinstance(obj, models.SocialEventLocation))
+
+    def test_from_fb_place(self):
+        fb_d = {
+            "name": "Coachella",
+            "location": {
+                "latitude": 33.679974,
+                "longitude": -116.237221
+            },
+            "id": "20281766647"
+        }
+        location = models.LocationFactory.from_fb_place(fb_d)
+        result = location.to_dict()
+        expected_d = {
+            'obj_type': "SocialEventLocation",
+            'coordinates': {
+                "latitude": 33.679974,
+                "longitude": -116.237221
+            },
+            'eventName': 'Coachella',
+            'address': 'Indio, CA, USA',
+        }
+        self.assertDictEqual(expected_d, result)
