80cf7f09153e841a4489a7bb56e362c308d1d1bf
billyrrr <billrao@me.com>
2020-04-04 12:43:12 -0700

    Switch to CTX.load for context initialization

--- gravitate/__init__.py
+++ gravitate/__init__.py
@@ -1,17 +1,5 @@
-# import sys
-# sys.path.append('../gravitate')
-
-import os
-
 from flask_boiler import context
 from flask_boiler import config
 
-Config = config.Config
-
-testing_config = Config(app_name="gravitate-backend-testing",
-                        debug=True,
-                        testing=True,
-                        certificate_path=os.path.curdir + "/gravitate/config_jsons/gravitate-backend-testing-firebase-adminsdk-nztgj-d063415ecc.json")
-
 CTX = context.Context
-CTX.read(testing_config)
+CTX.load()
